a15f001e7ebffa7eded87e998e325ea64ad0c85c
kchikemorah <c.kene@wustl.edu>
2024-05-17 11:38:33 -0500

    php

---
+++ index.php
@@ -0,0 +1,93 @@
+
+<html>
+    <head>
+        <title> Kene's Adult Life! </title> <!--style and title always in the head-->
+        <link rel="stylesheet" href="style.css">
+        <style>
+            .dropbtn{
+                background-color: darkolivegreen;
+                color: forestgreen;
+                border: ridge;
+                border-radius: 15px;
+            }
+            .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: lightgray;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+            }
+
+            .dropdown-content a {
+            padding:  5px;
+            text-decoration: none;
+            display: block;
+}
+
+            .dropdown-content a:hover {background-color: darkolivegreen;}
+
+            .dropdown:hover .dropdown-content {display: block;}
+
+            .dropdown:hover .dropbtn {background-color: forestgreen;} 
+        </style>
+    </head>
+
+    <body style="background-color: darkseagreen"> 
+        <div style="color:cadetblue; font-size: 100px; border: 10px solid forestgreen; border-style: ridge;" > Who is Kene?  <span style = "color:darkolivegreen; font-size: 20px" > Computer Science + Math, (and a minor in lingustics)</span></div>
+        <br>
+        <br>
+        <div style="color:cadetblue; font-size: 30px;"><strong><center>Hello! I am 19 years old (almost 20!), and over the past week I have: </center></strong></div>
+        <br>
+        <br>
+        <p> 1. Moved into a house (temporarily) </p>
+        <img src="unpack.jpg" alt = "unpack" style="width:50%;height:70%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px">  </img>
+        <p> 2. Bought my first car </p>
+        <img src="car.jpg" alt = "first car" style="width:50%;height:70%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px">  </img>
+        <img src="signing.jpg" alt = "signing my first car" style="float: right;width:40%;height:60%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px">  </img>
+        <p> 3. (consequently to 2) parallel parked in St. Louis </p>
+        <img src="parallel.jpg" alt = "parallel parking" style="width:50%;height:70%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px">  </img>
+        <p> 4. Paid my first month of auto insurance</p>
+        <img src="progressive.png" alt = "insurance" style="width:30%;height:30%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px">  </img>
+        <p> 5. Survived my first day of work!!!!! </p>
+        <h2 style="border: 10px solid cadetblue; border-style: ridge; border-radius: 20px;"><center> Miscellaneous</center></h2> 
+        <p>
+        I also enjoy:
+        <br>
+        legos
+        <img src="up house.jpg" alt = "up house lego set" style="float:right;width:30%;height:30%; border: 10px solid darkolivegreen; border-style: double; border-radius: 10px"> 
+        <br>
+        origami
+        <br>
+         </p>
+        <div class="dropdown">
+            <button class="dropbtn"><p>long tv shows</p></button>
+            <div class="dropdown-content">
+              <a href="https://onceuponatime.fandom.com/wiki/Once_Upon_a_Time_Wiki"><p> Once Upon a Time</p></a>
+              <a href="https://marvelcinematicuniverse.fandom.com/wiki/Agents_of_S.H.I.E.L.D."><p> Agents of Shield</p></a>
+              <a href="https://avatar.fandom.com/wiki/Avatar_Wiki"><p>Avatar: The Last Airbender</p></a>
+            </div>
+          </div>
+        <br> 
+        <p>
+        <a href="https://docs.google.com/document/d/1aFbIkOn6DvvCzDB8tAYdjFlnS4k-TuSZFVUiRtUPWwI/edit?usp=sharing" target="_blank"><p>reading</p></a>
+        
+        <a href="poetry.php" target="_blank"> <p>spoken word poetry</p></a>
+ 
+        </p>
+       
+
+        <!-- it is very easy to steal front end code, so 
+            we dont comment here where ppl can see it, only in backend! -->
+        <!--<a href="https://www.linkedin.com/in/kene-chike-morah-532715225" target="_blank">LinkedIn </a>
+        <br/>
+        <br/>  -->
+
+       
+       
+
+        
+    </body>
+
+
+</html>
