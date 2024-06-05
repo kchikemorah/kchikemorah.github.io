c7c81bd2f84e9dd76bb47f168d401d944e6ce594
kchikemorah <c.kene@wustl.edu>
2024-06-05 13:30:57 -0500

    updated

--- index.php
+++ index.php
@@ -6,15 +6,29 @@
         </script>
         
     </head>
-    <body style='margin:0';>
+    <body style='margin:0; background-color: lightbrown'>
         <div style='background-color: white; height: 60px;'></div>
-        <div class='conveyorBelt'></div>
-        <div class = 'containers'>
-            <div class='bin'> bin 1</div>
-            <div class='bin'> bin 2</div>
-            <div class='bin'> bin 3</div>
-            <div class='bin'> bin 4</div>
+
+        <div class='conveyorBelt'>
+            <?php
+            
+             for($i=1; $i<15; $i++){
+                echo" <div class='beltRolls'></div>";
+            }
+            ?>
+            <div class='item1'></div>
+            <div class='item2'></div>
+            <div class='item3'></div>
+            <div class='item4'></div>
         </div>
+
+        <div class = 'containers'>
+            <div class='bin1'> bin 1</div>
+            <div class='bin2'> bin 2</div>
+            <div class='bin3'> bin 3</div>
+            <div class='bin4'> bin 4</div>
+        </div>
+
         <div class='player'>
             <div class='playerHead'></div>
             <div class='playerBody'>
