2cb6501f194e7945cc4e5abc98820facaf8e0b6d
kchikemorah <c.kene@wustl.edu>
2024-06-05 10:10:54 -0500

    so far

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-stuff
+<html>
+    <head>
+    <title> Play </title>
+    <link rel='stylesheet' href='gamestyle.css'>
+        <script>
+        </script>
+        
+    </head>
+    <body style='margin:0';>
+        <div style='background-color: white; height: 60px;'></div>
+        <div class='conveyorBelt'></div>
+        <div class = 'containers'>
+            <div class='bin'> bin 1</div>
+            <div class='bin'> bin 2</div>
+            <div class='bin'> bin 3</div>
+            <div class='bin'> bin 4</div>
+        </div>
+        <div class='player'>
+            <div class='playerHead'></div>
+            <div class='playerBody'>
+                <div class='spine'></div>
+                <div class='leftHand'></div>
+                <div class='rightHand'></div>
+        </div>
+    </body>
+</html>
